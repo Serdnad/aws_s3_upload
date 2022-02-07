a3accfc75790e7209f2f58b4236090234c3fe81f
jasonroland <1jasonroland@gmail.com>
2022-02-07 18:41:56 -0500

    Delete aws_s3_upload-1.3.0 directory

--- aws_s3_upload-1.3.0/test/aws_s3_upload_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-// TODO: Write Tests(?) May be difficult without exposing credentials
-void main() {
-  test('check test run', () {
-    expect(true, true);
-  });
-}
