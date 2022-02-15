03cb33978ef3fbf2ed68a27a1b3a77180f44eee9
Jason Roland <1jasonroland@gmail.com>
2022-02-15 15:14:48 -0500

    uploaded correctly

--- lib/aws_s3_upload.dart
+++ lib/aws_s3_upload.dart
@@ -1,8 +1,9 @@
 library aws_s3_upload;
 
 import 'dart:io';
-
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
+import 'package:aws_s3_upload/enum/acl.dart';
+import 'package:aws_s3_upload/src/utils.dart';
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 
@@ -30,6 +31,10 @@ class AwsS3 {
     /// The AWS region. Must be formatted correctly, e.g. us-west-1
     String region = 'us-east-2',
 
+    ///Access control list enables you to manage access to bucket and objects
+    ///For more information visit [https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl-overview.html]
+    ACL acl = ACL.public_read,
+
     /// The filename to upload as. If null, defaults to the given file's current filename.
     String? filename,
   }) async {
@@ -41,15 +46,19 @@ class AwsS3 {
 
     final uri = Uri.parse(endpoint);
     final req = http.MultipartRequest("POST", uri);
-    final multipartFile = http.MultipartFile('file', stream, length, filename: path.basename(file.path));
+    final multipartFile = http.MultipartFile('file', stream, length,
+        filename: path.basename(file.path));
 
-    final policy = Policy.fromS3PresignedPost(uploadDest, bucket, accessKey, 15, length, region: region);
-    final key = SigV4.calculateSigningKey(secretKey, policy.datetime, region, 's3');
+    final policy = Policy.fromS3PresignedPost(
+        uploadDest, bucket, accessKey, 15, length, acl,
+        region: region);
+    final key =
+        SigV4.calculateSigningKey(secretKey, policy.datetime, region, 's3');
     final signature = SigV4.calculateSignature(key, policy.encode());
 
     req.files.add(multipartFile);
     req.fields['key'] = policy.key;
-    req.fields['acl'] = 'public-read';
+    req.fields['acl'] = aclToString(acl);
     req.fields['X-Amz-Credential'] = policy.credential;
     req.fields['X-Amz-Algorithm'] = 'AWS4-HMAC-SHA256';
     req.fields['X-Amz-Date'] = policy.datetime;
@@ -58,7 +67,7 @@ class AwsS3 {
 
     try {
       final res = await req.send();
-
+      
       if (res.statusCode == 204) return '$endpoint/$uploadDest';
     } catch (e) {
       print('Failed to upload to AWS, with exception:');
--- lib/src/policy.dart
+++ lib/src/policy.dart
@@ -1,32 +1,38 @@
 import 'dart:convert';
 
 import 'package:amazon_cognito_identity_dart_2/sig_v4.dart';
+import 'package:aws_s3_upload/enum/acl.dart';
+import 'package:aws_s3_upload/src/utils.dart';
 
 class Policy {
   String expiration;
   String region;
+  ACL acl;
   String bucket;
   String key;
   String credential;
   String datetime;
   int maxFileSize;
 
-  Policy(this.key, this.bucket, this.datetime, this.expiration, this.credential, this.maxFileSize,
-      {this.region = 'us-east-1'});
+  Policy(this.key, this.bucket, this.datetime, this.expiration, this.credential,
+      this.maxFileSize, this.acl,
+      {this.region = 'us-east-2'});
 
-  factory Policy.fromS3PresignedPost(
-    String key,
-    String bucket,
-    String accessKeyId,
-    int expiryMinutes,
-    int maxFileSize, {
-    String region = 'us-east-1',
-  }) {
+  factory Policy.fromS3PresignedPost(String key, String bucket,
+      String accessKeyId, int expiryMinutes, int maxFileSize, ACL acl,
+      {String region = 'us-east-2'}) {
     final datetime = SigV4.generateDatetime();
-    final expiration = (DateTime.now()).add(Duration(minutes: expiryMinutes)).toUtc().toString().split(' ').join('T');
-    final cred = '$accessKeyId/${SigV4.buildCredentialScope(datetime, region, 's3')}';
+    final expiration = (DateTime.now())
+        .add(Duration(minutes: expiryMinutes))
+        .toUtc()
+        .toString()
+        .split(' ')
+        .join('T');
+    final cred =
+        '$accessKeyId/${SigV4.buildCredentialScope(datetime, region, 's3')}';
 
-    return Policy(key, bucket, datetime, expiration, cred, maxFileSize, region: region);
+    return Policy(key, bucket, datetime, expiration, cred, maxFileSize, acl,
+        region: region);
   }
 
   String encode() {
@@ -41,7 +47,7 @@ class Policy {
   "conditions": [
     {"bucket": "${this.bucket}"},
     ["starts-with", "\$key", "${this.key}"],
-    {"acl": "public-read"},
+    {"acl": "${aclToString(acl)}"},
     ["content-length-range", 1, ${this.maxFileSize}],
     {"x-amz-credential": "${this.credential}"},
     {"x-amz-algorithm": "AWS4-HMAC-SHA256"},
